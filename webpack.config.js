b5288a9cfde4aeb7332548da7680a34ee3d907e5
Vlad Gonchar <vladik.gonchar2015@gmail.com>
2021-07-24 21:08:56 +0300

    Готовый сборщик Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'my-webpack-setup.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'src/index.html', inject: 'body' }),
+        new MiniCssExtractPlugin({ filename: 'style.css' }),
+    ],
+    devServer: {
+        port: 4444,
+        open: true,
+        stats: 'errors-only',
+    },
+};
